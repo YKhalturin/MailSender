cead6adc83b4ba3772aeafb70ab395807f4da45e
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-09-23 21:26:09 +0300

    Initial commit Rename Project, Methods etc

---
+++ MailSender/WpfMailSender.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfTestMailSender
+{
+    /// <summary>
+    /// Interaction logic for WpfMailSender.xaml
+    /// </summary>
+    public partial class WpfMailSender : Window
+    {
+        public WpfMailSender()
+        {
+            InitializeComponent();
+        }
+    }
+}
