0b27a1a747a9ef8a54149b1c540211ffdef6c113
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-09-24 18:29:20 +0300

    Created working programm

---
+++ MailSender/MailSenderData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WpfTestMailSender
+{
+    public static class MailSenderData
+    {
+        public static string SmtpAdress = "smtp.yandex.ru";
+        public static int SmtpPort = 587;
+    }
+}
--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.Net.Mail;
 
 namespace WpfTestMailSender
 {
@@ -10,6 +13,24 @@ namespace WpfTestMailSender
         public WpfMailSender()
         {
             InitializeComponent();
+        }
+
+        private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            var credentials = new NetworkCredential
+            {
+                UserName = userNameBox.Text,
+                Password = passwordBox.Password
+            };
+            var emailData = new EmailSendService.Email
+            {
+                From = userNameBox.Text,
+                To = toBox.Text,
+                Subject = subjectBox.Text,
+                Body = bodyBox.Text
+            };
+            var email = new EmailSendService(credentials, emailData);
+            email.SendMessage();
         }
     }
 }
