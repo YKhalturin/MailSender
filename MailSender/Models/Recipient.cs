d464e47a58023d72d010f5ff21206d562b3d207b
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-09-28 11:45:35 +0300

    Created Models

---
+++ MailSender/Models/Recipient.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfTestMailSender.Models
+{
+    public class Recipient
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ MailSender/Models/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WpfTestMailSender.Models
+{
+    public class Server
+    {
+        public string Address { get; set; }
+
+        private int _port;
+        public int Port
+        {
+            get => _port;
+            set
+            {
+                if(value < 0 || value>= 65535)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапазоне от 0 до 65534");
+            }
+        }
+        public bool UseSSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
