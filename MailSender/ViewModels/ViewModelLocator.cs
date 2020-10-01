6baed7001470a8e2b42f6086f3968589e86bcfb1
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-01 12:12:25 +0300

    Created ViewModel

---
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MailSender;
+
+namespace WpfTestMailSender.ViewModels
+{
+    class ViewModelLocator
+    {
+        //public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
