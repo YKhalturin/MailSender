396d05dfbad7eec2a1deff1f49501109f3274183
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-04 19:06:56 +0300

    Created RecipientView

--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -17,36 +17,36 @@ namespace WpfMailSender
             InitializeComponent();
         }
 
-        private void SendButton_Click(object Sender, RoutedEventArgs e)
-        {
-            if (!(ServersList.SelectedItem is Server server)) return;
-            if (!(SendersList.SelectedItem is Sender sender)) return;
-            if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
-            if (!(MessagesList.SelectedItem is Message message)) return;
+        //private void SendButton_Click(object Sender, RoutedEventArgs e)
+        //{
+        //    if (!(ServersList.SelectedItem is Server server)) return;
+        //    if (!(SendersList.SelectedItem is Sender sender)) return;
+        //    if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
+        //    if (!(MessagesList.SelectedItem is Message message)) return;
 
-            var sendService = new MailSenderService
-            {
-                ServerAddress = server.Address,
-                ServerPort = server.Port,
-                UseSSL = server.UseSSL,
-                Login = server.Login,
-                Password = server.Password,
-            };
-            try
-            {
-                var emailData = new MailSenderService.Email
-                {
-                    SenderAddress = sender.Address,
-                    RecipientAddress = recipient.Address,
-                    Subject = message.Subject,
-                    Body = message.Body
-                };
-                sendService.SendMessage(emailData);
-            }
-            catch (SmtpException ex)
-            {
-                MessageBox.Show($"Ошибка при отправке почты {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
+        //    var sendService = new MailSenderService
+        //    {
+        //        ServerAddress = server.Address,
+        //        ServerPort = server.Port,
+        //        UseSSL = server.UseSSL,
+        //        Login = server.Login,
+        //        Password = server.Password,
+        //    };
+        //    try
+        //    {
+        //        var emailData = new MailSenderService.Email
+        //        {
+        //            SenderAddress = sender.Address,
+        //            RecipientAddress = recipient.Address,
+        //            Subject = message.Subject,
+        //            Body = message.Body
+        //        };
+        //        sendService.SendMessage(emailData);
+        //    }
+        //    catch (SmtpException ex)
+        //    {
+        //        MessageBox.Show($"Ошибка при отправке почты {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        //    }
+        //}
     }
 }
