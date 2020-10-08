f0f5d0e3bd9e37613f68f96127b765310d7b9406
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-08 11:05:04 +0300

    Created calculate factorial and sum using threads

---
+++ ThreadTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        public static long TotalAmount = 0;
+        public static long Factorial = 1;
+
+        static int Menu()
+        {
+            int m;
+            do
+            {
+                Console.WriteLine("1 - Вычисляем сумму неотрицательных числел до N. Многопоточно");
+                Console.WriteLine("2 - Вычисляем факториал до N. Многопоточно");
+                Console.WriteLine("0 - Выход");
+                m = Convert.ToInt32(Console.ReadLine());
+            }
+            while (m < 0 && m > 4);
+            return m;
+        }
+
+        static void Main(string[] args)
+        {
+            int menu;
+            do
+            {
+                menu = Menu();
+                switch (menu)
+                {
+                    case 1:
+                        ThreadTest.Task1();
+                        break;
+                    case 2:
+                        ThreadTest.Task2();
+                        break;
+                    default:
+                        Console.WriteLine("Bye!");
+                        break;
+                }
+            }
+            while (menu != 0);
+        }
+    }
+}
---
+++ ThreadTest/TreadsLauncher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    public class TreadsLauncher
+    {
+        static object locker = new object();
+        private static int _amountOfNumbersForSingleThreads = 5;
+        public static void LaunchThreadsToCalculateSum(int number)
+        {
+            PositiveNumbers positiveNumbers;
+            if (number < _amountOfNumbersForSingleThreads)
+            {
+                positiveNumbers = new PositiveNumbers(1, number);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(positiveNumbers.Sum));
+            }
+            else
+            {
+                for (int i = 1; i <= number; i += _amountOfNumbersForSingleThreads)
+                {
+                    if (number - i < _amountOfNumbersForSingleThreads) positiveNumbers = new PositiveNumbers(i, number);
+                    else
+                    {
+                        positiveNumbers = new PositiveNumbers(i, i + 4);
+                    }
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(positiveNumbers.Sum));
+                }
+            }
+        }
+
+        public static void LaunchThreadsToCalculateProductOfNumber(int number)
+        {
+            Factorial factorial;
+            if (number < _amountOfNumbersForSingleThreads)
+            {
+                factorial = new Factorial(1, number);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(factorial.ProductOfNumbers));
+            }
+            else
+            {
+                for (int i = 1; i <= number; i += _amountOfNumbersForSingleThreads)
+                {
+                    if (number - i < _amountOfNumbersForSingleThreads) factorial = new Factorial(i, number);
+                    else
+                    {
+                        factorial = new Factorial(i, i + 4);
+                    }
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(factorial.ProductOfNumbers));
+                }
+            }
+        }
+    }
+}
