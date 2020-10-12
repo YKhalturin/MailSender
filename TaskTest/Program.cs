360780f0e5386aed75b3865251e1a11491a4ec7b
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-12 18:24:47 +0300

    Created Multiplication of 2 matrix using tap

---
+++ TaskTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Матрица А");
+            await Matrix.Print(await Matrix.A);
+            Console.WriteLine($"A. TaskId:{Task.CurrentId}");
+            Console.WriteLine("Матрица B");
+            await Matrix.Print(await Matrix.B);
+            Console.WriteLine($"B. TaskId:{Task.CurrentId}");
+            Console.WriteLine("Матрица C рассчитывается...");
+            var result = await Matrix.Multiplication(await Matrix.A, await Matrix.B);
+            await Matrix.Print(result);
+
+            Console.WriteLine("Перемножение матрицы завершено");
+            Console.ReadKey();
+        }
+    }
+}
