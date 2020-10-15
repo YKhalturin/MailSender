eaa8de6ded52cde30e414434636d96b4a6f9da1c
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-15 16:25:21 +0300

    Added Cinema wpf test

---
+++ CinemaWpfTest/DatabaseContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace CinemaWpfTest
+{
+    public class DatabaseContainer : DbContext
+    {
+        public DatabaseContainer()
+            : base("DbConnection")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
