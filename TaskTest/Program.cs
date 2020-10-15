962fcd6d27a90456bff6864b040f61d3f8e14e95
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-15 08:49:06 +0300

    remove await from print

--- TaskTest/Program.cs
+++ TaskTest/Program.cs
@@ -12,14 +12,14 @@ namespace TaskTest
         static async Task Main(string[] args)
         {
             Console.WriteLine("Матрица А");
-            await Matrix.Print(await Matrix.A);
+            Matrix.Print(await Matrix.A);
             Console.WriteLine($"A. TaskId:{Task.CurrentId}");
             Console.WriteLine("Матрица B");
-            await Matrix.Print(await Matrix.B);
+            Matrix.Print(await Matrix.B);
             Console.WriteLine($"B. TaskId:{Task.CurrentId}");
             Console.WriteLine("Матрица C рассчитывается...");
             var result = await Matrix.Multiplication(await Matrix.A, await Matrix.B);
-            await Matrix.Print(result);
+            Matrix.Print(result);
 
             Console.WriteLine("Перемножение матрицы завершено");
             Console.ReadKey();
