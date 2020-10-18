6ee3f523e11fd890d86a8e880427475fc0567bd7
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-10-18 12:04:30 +0300

    Creted task 3 from lesson 8

---
+++ SQLQueryTask3-Lesson8.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE[dbo].[Users] (
+    [Id] INT IDENTITY(1, 1) NOT NULL,
+    [Name] NVARCHAR(10)  
+    );
+
+    DELETE FROM [dbo].[Users];
+
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Albert');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Albert');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Albert');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Andrey');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Andrey');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Nikolay');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Alex');
+    INSERT INTO [dbo].[Users] (Name) VALUES ('Artem');
+
+ SELECT Name, COUNT(Name) as cnt
+    FROM [dbo].[Users]
+    WHERE Name Like 'A%'
+    GROUP BY Name
+    HAVING COUNT(Name) > 1
+
